d6164faeb3cec9ef035edd04a9c2a69e5ea38a8a
Thad Stinson <bootytee@Thads-iMac.local>
2019-11-04 10:01:53 -0600

    updated header and index

--- whirlwind_winery_site/src/pages/index.js
+++ whirlwind_winery_site/src/pages/index.js
@@ -7,10 +7,10 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Whirlwind Winery" />
+    <h1>Whirlwind Winery</h1>
+    <p>Welcome to Whirlwind Winery!</p>
+    <p>The best wine in Watonga.  Garaunteed!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
